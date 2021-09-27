db045d1ff56abb92e9d8c935788252c4d3bbcb8b
jonhall <jonhall@us.ibm.com>
2021-09-27 11:51:37 -0500

    Integrate Platform Services SDK

--- invoiceAnalysis/forms.py
+++ invoiceAnalysis/forms.py
@@ -1,6 +1,6 @@
-from wtforms import Form, StringField, validators
+from wtforms import Form, StringField, validators, PasswordField
 
 class InvoiceAnalysisRequest(Form):
-    ic_api_key = StringField('IBM Cloud ApiKey:', [validators.Length(min=4, max=45)])
+    ic_api_key = PasswordField('IBM Cloud ApiKey:', [validators.Length(min=4, max=45)])
     month = StringField("Invoice month (YYYY-MM):", [validators.Length(min=7, max=7)])
 
