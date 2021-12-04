c70e1b001f8ce6c0339002ae371d4ad5f8dee4ce
Jon Hall <jon@hallusa.com>
2021-12-04 11:43:04 -0600

    update

--- invoiceAnalysis.py
+++ invoiceAnalysis.py
@@ -39,6 +39,7 @@ else:
     # extract from codeengine environment
     redis_connection = json.load(os.environ.get('DATABASES_FOR_REDIS_CONNECTION'))
     cert_name = redis_connection["cli"]["certificate"]["name"]
+    app.config["broker_transport"] = "rediss"
     app.config['broker_url'] =  redis_connection["cli"]["arguments"][1] + "?ssl_cert_reqs=required&ssl_ca_certs=/certs/" + cert_name
     app.config['result_backend'] = redis_connection["cli"]["arguments"][1] + "?ssl_cert_reqs=required&ssl_ca_certs=/certs/" + cert_name
 
