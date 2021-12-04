c45e56912430a792652393e46aa556be218d3a83
Jon Hall <jon@hallusa.com>
2021-12-04 13:44:07 -0600

    update

--- invoiceAnalysis.py
+++ invoiceAnalysis.py
@@ -41,7 +41,7 @@ else:
     cert_name = redis_connection["cli"]["certificate"]["name"]
     app.config["broker_transport"] = "rediss"
     app.config['broker_url'] =  redis_connection["cli"]["arguments"][0][1] + "?ssl_cert_reqs=required&ssl_ca_certs=/certs/" + cert_name
-    app.config['result_backend'] = redis_connection["cli"]["arguments"][0][1] + "?ssl_cert_reqs=required&ssl_ca_certs=/certs/" + cert_name
+    #app.config['result_backend'] = redis_connection["cli"]["arguments"][0][1] + "?ssl_cert_reqs=required&ssl_ca_certs=/certs/" + cert_name
 
 celery = Celery(app.name, broker=app.config['broker_url'])
 celery.conf.update(app.config)
